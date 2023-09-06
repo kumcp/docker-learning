235ff1bc50eec290139668e6b78609184e4d0095
kum_cp <kum_cp@yahoo.com.vn>
2023-09-06 19:28:43 +0700

    update lab

--- lab/lab2/lab21/script.sh
+++ lab/lab2/lab21/script.sh
@@ -7,3 +7,6 @@ docker run -p 81:80 -d lab:2.1.0
 
 # check inside container
 docker exec -it hungry_jackson /bin/bash
+
+
+docker build -t lab:2.2.1 --target dev-runner  .
--- lab/lab3/script.sh
+++ lab/lab3/script.sh
@@ -5,8 +5,10 @@ docker build -t lab3:1.0.0 -f Dockerfile .
 # Open port 82 for testing
 docker run -p 82:80  lab3:1.0.0
 
+docker run -p 82:80 -v vol1:/usr/share/nginx/html/ lab3:1.0.0
+
 # Mount a file from host to container
-docker run --name ctn-3.4 -v /home/kum/projects/codestar/docker-demo/lab/practice4/nginx-config.conf:/etc/nginx/conf.d/default.conf  -td lab4:1.0.3
+docker run --name ctn-3.4 -v /home/kum/projects/codestar/docker-demo/lab/practice4/nginx-config.conf:/etc/nginx/conf.d/default.conf  -t lab4:1.0.3
 
 # Mount from a volume to container
 docker run -p 82:80 -v c004803578c849ffde8ede0bea2b011521aace098c7e7f840566d6200a6938cc:/usr/share/nginx/html/ lab3:2.0.0
